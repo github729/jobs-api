e7f23334f672b5d52105df743bc6237380bfafaf
Anusha Manne <anushamanne7@gmail.com>
2019-05-25 10:15:49 +0530

    facebook setup

--- router.js
+++ router.js
@@ -9,11 +9,28 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     res.send("The Jobs Api watch at 1332");
   });
+
+  app.get('/fb', (req, res) => {
+    var FB = require('fb');
+    FB.setAccessToken('EAACEdEose0cBAJjLjbUmgEgD0BzDxHJltFyLseyBX1mPoRURXNZCvhcbfD88H65l2hLxwbKhEAiyNwUN8Nq3p813opeboTaMFHwY0Ww3Nv0jSstXB5em6xDQwU0gZBaNf9qNcnDBe1QTAqpAntQZCcZB7ZBljeifZB87tK6MmLnS5sNvaDwdeq5nHZASobw1TNSBbuGf1NmpQZDZD');
+    var body = 'My first post using facebook-node-sdk';
+    FB.api('me/feed', 'post', { message: body, link: 'http://ec2-35-178-115-252.eu-west-2.compute.amazonaws.com/insights/preview/23' }, function (res) {
+        if (!res || res.error) {
+            console.log(!res ? 'error occurred' : res.error);
+            return;
+        }
+    });
+    res.json({ 'hi': res.id });
+});
+
+accessToken = null;
+
   var apiRoutes = express.Router();
 
   apiRoutes.get("/", (req, res) => {
     res.send("Welcome to Jobs Api");
   });
+
   //user urls
   apiRoutes.post("/sign-up", UserController.Register);
   apiRoutes.post("/sign-in", UserController.Login);
